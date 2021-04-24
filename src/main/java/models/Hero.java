34061b6d172301cbaa16cb043ce48d024b37e81d
Brian <bweloba@gmail.com>
2021-04-24 11:55:12 +0300

    add constructor with squad id

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -5,6 +5,16 @@ public class Hero {
     int mAge;
     String mPower;
     String mWeakness;
+    int squadId;
+
+    public Hero(String name, int age, String power, String weakness, int id) {
+        this.mName = name;
+        this.mAge = age;
+        this.mPower = power;
+        this.mWeakness = weakness;
+        this.squadId = id;
+    }
+
 
     public Hero(String name, int age, String power, String weakness) {
         this.mName = name;
