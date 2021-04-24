a60f7f2c91c2e13ea63b78b03eff288d41d1cd8f
Brian <bweloba@gmail.com>
2021-04-24 11:23:57 +0300

    add routes to form submission

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,13 @@ import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
+import java.util.Map;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
         //path to hope page
         get("/", (request, response) -> {
             return new ModelAndView(new HashMap<String, Object>(), "home.hbs");
@@ -17,6 +19,18 @@ public class App {
             return new ModelAndView(new HashMap<String, Object>(), "hero_form.hbs");
         }, new HandlebarsTemplateEngine());
 
-
+        //display heroes
+        get("/heroes", (request, response) -> {
+            Map<String,Object> model = new HashMap<String,Object>();
+            String heroName= request.queryParams("hero-name");
+            int heroAge= Integer.parseInt(request.queryParams("hero-age"));
+            String heroPower= request.queryParams("hero-power");
+            String heroWeakness= request.queryParams("hero-weakness");
+            model.put("hero-name",heroName);
+            model.put("hero-age", heroAge);
+            model.put("hero-power", heroPower);
+            model.put("hero-weakness", heroWeakness);
+            return  new ModelAndView(model,"heroes.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
