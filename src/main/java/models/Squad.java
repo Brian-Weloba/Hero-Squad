46523d57131402361b29b6c7981bcf7e6b768fb7
Brian <bweloba@gmail.com>
2021-04-27 12:02:18 +0300

    add tests for Squad,java

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,11 +1,37 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Squad {
+    public static ArrayList<Squad> squads = new ArrayList<>();
+    public ArrayList<Hero> squadHeroes = new ArrayList<>();
     String squadName;
     int id;
+    private String fightingCause;
+    private int size;
 
-    public Squad(String name){
-        this.squadName=name;
+    public Squad(String name) {
+        this.squadName = name;
+    }
+
+    public Squad(String squadName, String fightingCause, int size) {
+        this.squadName = squadName;
+        this.fightingCause = fightingCause;
+        this.size = size;
+        squads.add(this);
+        this.id = squads.size();
+    }
+
+    public static ArrayList<Squad> getSquads() {
+        return squads;
+    }
+
+    public static void clearAllSquads() {
+        squads.clear();
+    }
+
+    public static Squad findById(int id) {
+        return squads.get(id - 1);
     }
 
     public String getSquadName() {
