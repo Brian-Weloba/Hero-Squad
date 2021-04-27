474e418881b0d9bf4e2258040ef75f86a06bff65
Brian <bweloba@gmail.com>
2021-04-27 12:03:53 +0300

    add tests for squad object

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -2,13 +2,39 @@ package models;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SquadTest {
 
     @Test
     public void SquadInstantiatesWithContent_true() {
         Squad squad = new Squad("Avengers");
-        assertEquals("Avengers",squad.getSquadName());
+        assertEquals("Avengers", squad.getSquadName());
     }
+
+    @Test
+    public void all_ReturnsAllInstancesOfSquads_true() {
+        Squad squad = new Squad("Avengers", "World Peace", 5);
+        Squad squad2 = new Squad("Hydra", "World Domination", 7);
+        assertTrue(Squad.getSquads().contains(squad2));
+        assertTrue(Squad.getSquads().contains(squad));
+
+    }
+
+    @Test
+    public void findReturnsCorrectSquad() {
+        Squad.clearAllSquads();
+        Squad squad = new Squad("Avengers", "World Peace", 5);
+        assertEquals(1, Squad.findById(squad.getId()).getId());
+    }
+
+    @Test
+    public void findReturnsCorrectlyWhenMoreThanOneSquadExists() {
+        Squad.clearAllSquads();
+        Squad squad = new Squad("Avengers", "World Peace", 5);
+        Squad squad2 = new Squad("Hydra", "World Domination", 7);
+        assertEquals(2, Squad.findById(squad2.getId()).getId());
+    }
+
 }
